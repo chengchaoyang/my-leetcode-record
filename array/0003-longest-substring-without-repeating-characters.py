be2569c6d140184924a30f384ff0994fb93663df
chengchaoyang <526879530@qq.com>
2020-04-08 23:03:15 +0800

    hash-table

--- array/0003-longest-substring-without-repeating-characters.py
+++ array/0003-longest-substring-without-repeating-characters.py
@@ -20,21 +20,89 @@ https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
 """
 
 class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        hash = {}
-        max_size = 0
-        begin = -1
-        for i,t in enumerate(s):
-            if t not in hash:
-                max_size = max(i - begin, max_size)
-            else:
-                begin = hash[t]+1
-                max_size = max(i - begin, max_size)
-            hash[t] = i
-            print(max_size)
-            print(hash)
-        return max_size
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
 
+        # 特判
+        size = len(s)
+        if size < 2:
+            return size
+
+        l = 0
+        r = -1
+
+        counter = [0 for _ in range(256)]
+
+        res = 1
+        while l < size:
+            if r + 1 < size and counter[ord(s[r + 1])] == 0:
+                # 表示没有重复元素，r 可以加 1
+                counter[ord(s[r + 1])] += 1
+                r += 1
+            else:
+                # 有重复元素，左边就要减 1
+                counter[ord(s[l])] -= 1
+                l += 1
+            res = max(res, r - l + 1)
+        return res
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        # 特判
+        l = len(s)
+        if l < 2:
+            return l
+        # 隔板法
+        # key:字符，val 出现的索引
+        map = dict()
+        point = 0
+        res = 1
+        for i in range(l):
+            # 关键1：map[s[i]] >= point，等于是可以的
+            if s[i] in map and map[s[i]] >= point:
+                # 先把隔板向后移动一位
+                point = map[s[i]] + 1
+            # 然后记录最长不重复子串的长度
+            res = max(res, i - point + 1)
+            # 关键2：无论如何都更新位置
+            map[s[i]] = i
+        return res
+
+class Solution:
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        # 特判
+        l = len(s)
+        if l < 2:
+            return l
+        # dp[i] 表示以 s[i] 结尾的最长不重复子串的长度
+        # 因为自己肯定是不重复子串，所以初始值设置为 1
+        dp = [1 for _ in range(l)]
+        map = dict()
+        map[s[0]] = 0
+        for i in range(1, l):
+            if s[i] in map:
+                if i - map[s[i]] > dp[i - 1]:
+                    dp[i] = dp[i - 1] + 1
+                else:
+                    dp[i] = i - map[s[i]]
+            else:
+                dp[i] = dp[i - 1] + 1
+            # 设置字符与索引键值对
+            map[s[i]] = i
+        # 最后拉通看一遍最大值
+        return max(dp)
 
 s =  "abba"
 print(Solution().lengthOfLongestSubstring(s))
