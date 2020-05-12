2b06c4ee0eb8bc91f07f922689492283b312e6f6
chengzhaoyang <526879530@qq.com>
2020-05-12 18:44:56 +0800

    tree

---
+++ tree/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -0,0 +1,44 @@
+"""
+根据一棵树的前序遍历与中序遍历构造二叉树。
+
+注意:
+你可以假设树中没有重复的元素。
+
+例如，给出
+
+前序遍历 preorder = [3,9,20,15,7]
+中序遍历 inorder = [9,3,15,20,7]
+返回如下的二叉树：
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+from typing import List
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        if len(preorder) == 0:
+            return None
+        root = TreeNode(preorder[0])
+        ind = inorder.index(preorder[0])
+        #左子树有几个节点
+        left_size = ind
+        if left_size + 1 <= len(preorder):
+            root.left = self.buildTree(preorder[1:left_size+1],inorder[:ind])
+        if ind+1 <= len(inorder) - 1:
+            root.right = self.buildTree(preorder[left_size+1:],inorder[ind+1:])
+        return root
+
---
+++ tree/0109-convert-sorted-list-to-binary-search-tree.py
@@ -0,0 +1,62 @@
+"""
+给定一个单链表，其中的元素按升序排序，将其转换为高度平衡的二叉搜索树。
+
+本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
+
+示例:
+
+给定的有序链表： [-10, -3, 0, 5, 9],
+
+一个可能的答案是：[0, -3, 9, -10, null, 5], 它可以表示下面这个高度平衡二叉搜索树：
+
+      0
+     / \
+   -3   9
+   /   /
+ -10  5
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/convert-sorted-list-to-binary-search-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    """
+    重点是找到链表的中点
+    """
+    def sortedListToBST(self, head: ListNode) -> TreeNode:
+        if head is None:
+            return None
+        if head.next is None:
+            return TreeNode(head.val)
+        fast = head
+        slow = head
+        pre = None
+        while fast.next and fast.next.next:
+            fast = fast.next.next
+            pre = slow
+            slow = slow.next
+        if pre:
+            pre.next = None
+        root = TreeNode(slow.val)
+        right = slow.next
+        slow.next = None
+        # 当链表只有 2 个结点的时候，pre 指针此时为 None，不用递归构造左子树
+        if pre:
+            pre.next = None
+            root.left = self.sortedListToBST(head)
+        root.right = self.sortedListToBST(right)
+        return root
+
